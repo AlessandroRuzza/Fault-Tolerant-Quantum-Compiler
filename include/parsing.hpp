7f113feb79fea1c7aef8a9229bc9563e320b638f
FedericoQuartieri <federico.quartieri2002@gmail.com>
2026-03-12 14:18:08 +0100

    added flags(x,y,graph),  works but external graph dont work

--- include/parsing.hpp
+++ include/parsing.hpp
@@ -4,7 +4,26 @@
 #include <string>
 
 void print_usage(const char* executable);
-void apply_config_overrides(int argc, char **argv, std::string& path, std::string& strategy, std::string& type, std::string& config_path);
-void argument_parsing(int argc, char **argv, std::string& path, std::string& strategy, std::string& type);
+void apply_config_overrides(
+    int argc,
+    char **argv,
+    std::string& path,
+    std::string& strategy,
+    std::string& type,
+    std::string& config_path,
+    int& x,
+    int& y,
+    std::string& graph_path
+);
+void argument_parsing(
+    int argc,
+    char **argv,
+    std::string& path,
+    std::string& strategy,
+    std::string& type,
+    int& x,
+    int& y,
+    std::string& graph_path
+);
 
 #endif // PARSING_HPP
