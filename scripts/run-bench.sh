e6365de171e9c5abfaa2793b57982a05849241e9
FedericoQuartieri <federico.quartieri2002@gmail.com>
2026-04-21 14:37:02 +0200

    added custom mpi

--- scripts/run-bench.sh
+++ scripts/run-bench.sh
@@ -5,6 +5,8 @@ binary_path="${1:-}"
 bench_path="${2:-${BENCH_PATH:-}}"
 rerun_timeouts="${3:-${RERUN_TIMEOUTS:-}}"
 bench_jobs="${4:-${BENCH_JOBS:-}}"
+bench_process_count="${5:-${BENCH_PROCESS_COUNT:-}}"
+processor="${6:-${PROCESSOR:-}}"
 
 read_makeflag_var() {
     key="$1"
@@ -31,12 +33,36 @@ if [ -z "$bench_jobs" ]; then
     bench_jobs="$(read_makeflag_var BENCH_JOBS || true)"
 fi
 
+if [ -z "$bench_process_count" ]; then
+    bench_process_count="$(read_makeflag_var BENCH_PROCESS_COUNT || true)"
+fi
+
+if [ -z "$processor" ]; then
+    processor="$(read_makeflag_var PROCESSOR || true)"
+fi
+
+if [ -z "$processor" ]; then
+    processor="0"
+fi
+
 if [ -z "$binary_path" ] || [ -z "$bench_path" ]; then
-    echo "Usage: make run-bench BENCH_PATH=<path_or_name> [RERUN_TIMEOUTS=1] [BENCH_JOBS=<n>]"
+    echo "Usage: make run-bench BENCH_PATH=<path_or_name> [RERUN_TIMEOUTS=1] [BENCH_JOBS=<n>] [BENCH_PROCESS_COUNT=<n>] [PROCESSOR=<n>]"
     exit 1
 fi
 
 run_binary() {
+    set -- "$@" --bench_path "$bench_path"
+
+    if [ -n "$bench_process_count" ]; then
+        set -- "$@" --process-count "$bench_process_count"
+    fi
+
+    set -- "$@" --processor "$processor"
+
+    if [ -n "$rerun_timeouts" ] && [ "$rerun_timeouts" != "0" ] && [ "$rerun_timeouts" != "false" ] && [ "$rerun_timeouts" != "False" ]; then
+        set -- "$@" --rerun-timeouts
+    fi
+
     if [ -n "$bench_jobs" ]; then
         exec env OMP_NUM_THREADS="$bench_jobs" "$@"
     fi
@@ -44,8 +70,4 @@ run_binary() {
     exec "$@"
 }
 
-if [ -n "$rerun_timeouts" ] && [ "$rerun_timeouts" != "0" ] && [ "$rerun_timeouts" != "false" ] && [ "$rerun_timeouts" != "False" ]; then
-    run_binary "$binary_path" --bench_path "$bench_path" --rerun-timeouts
-else
-    run_binary "$binary_path" --bench_path "$bench_path"
-fi
+run_binary "$binary_path"
