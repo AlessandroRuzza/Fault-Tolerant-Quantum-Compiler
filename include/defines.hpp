e6e5fab66e99955a99df66a3b5207d7eba13b273
AlessandroRuzza <alessandroruzza22@gmail.com>
2026-04-23 12:27:47 +0200

    minor, add test.json

--- include/defines.hpp
+++ include/defines.hpp
@@ -17,25 +17,15 @@ extern const bool PRINT_ROUTING; // controls routing steps printing
 extern const bool PRINT_DRAW_ROUTING; // controls routing lattice drawing printing
 extern const bool PRINT_ROUTING_PROGRESS; // controls routing progress printing
 
+/*
+############ BEHAVIORAL FLAGS ######################
+*/
 extern const bool MAGIC_STOPS_ROUTE;
 extern const bool USE_S_GATES;
 
-} // namespace compiler_flags
+} 
 
 // Keep existing unqualified names to avoid touching all call sites.
 using namespace compiler_flags;
-
-// using compiler_flags::PRINT_PARSING;
-// using compiler_flags::PRINT_CIRCUIT;
-// using compiler_flags::PRINT_MAPPING;
-// using compiler_flags::PRINT_MAPPING_GRAPH;
-// using compiler_flags::MAPPING_VERBOSE;
-// using compiler_flags::PRINT_SAFE_PASSAGE;
-// using compiler_flags::PRINT_LAYER;
-// using compiler_flags::PRINT_ROUTING;
-// using compiler_flags::PRINT_DRAW_ROUTING;
-// using compiler_flags::PRINT_ROUTING_PROGRESS;
-// using compiler_flags::MAGIC_STOPS_ROUTE;
-// using compiler_flags::USE_S_GATES;
 
 #endif // DEFINES_HPP
