f79917d81cf5c2fad535badff202d76365a5efd3
AlessandroRuzza <alessandroruzza22@gmail.com>
2026-04-23 12:19:26 +0200

    Moved defines to .cpp to prevent massive recompilation

---
+++ src/defines.cpp
@@ -0,0 +1,22 @@
+#include "defines.hpp"
+
+namespace compiler_flags {
+
+const bool PRINT_PARSING = false;
+const bool PRINT_CIRCUIT = false;
+
+const bool PRINT_MAPPING = false;
+const bool PRINT_MAPPING_GRAPH = false;
+const bool MAPPING_VERBOSE = false;
+
+const bool PRINT_SAFE_PASSAGE = false;
+
+const bool PRINT_LAYER = false;
+const bool PRINT_ROUTING = false;
+const bool PRINT_DRAW_ROUTING = true;
+const bool PRINT_ROUTING_PROGRESS = false;
+
+const bool MAGIC_STOPS_ROUTE = true;
+const bool USE_S_GATES = false;
+
+}
