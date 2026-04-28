512954fe4af45ecbc4ab2d8c5da4fdbc4c4887f5
FedericoQuartieri <federico.quartieri2002@gmail.com>
2026-04-28 19:02:06 +0200

    modified qasm parser

--- src/defines.cpp
+++ src/defines.cpp
@@ -6,14 +6,14 @@ const bool PRINT_PARSING = false;
 const bool PRINT_CIRCUIT = false;
 
 const bool PRINT_MAPPING = false;
-const bool PRINT_MAPPING_GRAPH = true;
+const bool PRINT_MAPPING_GRAPH = false;
 const bool MAPPING_VERBOSE = false;
 
 const bool PRINT_SAFE_PASSAGE = false;
 
 const bool PRINT_LAYER = false;
 const bool PRINT_ROUTING = false;
-const bool PRINT_DRAW_ROUTING = true;
+const bool PRINT_DRAW_ROUTING = false;
 const bool PRINT_ROUTING_PROGRESS = false;
 
 /*
