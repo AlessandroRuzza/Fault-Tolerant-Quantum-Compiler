bc22526f3f4816e00ed166802173f01823eb08a7
FedericoQuartieri <federico.quartieri2002@gmail.com>
2026-06-20 18:57:14 +0200

    repo cleaning

--- plots_2.sh
+++
@@ -1,4 +0,0 @@
-python3 scripts/generate_benchmark_plots.py --csv gaussian_weight_tuning_coarse_cube_sweetspot_runs --gaussian
-python3 scripts/generate_benchmark_plots.py --csv gaussian_weight_tuning_coarse_sweetspot_runs --gaussian
-python3 scripts/generate_benchmark_plots.py --csv gaussian_weight_tuning_fine_cube_sweetspot_runs --gaussian
-python3 scripts/generate_benchmark_plots.py --csv gaussian_weight_tuning_fine_sweetspot_runs --gaussian
